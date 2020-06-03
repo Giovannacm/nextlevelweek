e917c7adc54b3a93e5864e27ce626b5f303caa08
Giovanna <giovanna-cm@hotmail.com>
2020-06-03 13:54:58 -0300

    Concluindo as telas de cadastros e de busca

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -23,11 +23,14 @@ function getCities(event) {
     
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufvalue}/municipios`
     
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         for(const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -37,3 +40,35 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(let item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items")
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        return item == itemId
+    })
+    
+    if(alreadySelected >= 0) {  //Se já estiver selecionado, retira a seleção
+        const filteredItems = selectedItems.filter( item => {
+            return item != itemId
+        })
+        
+        selectedItems = filteredItems
+    }
+    else { //Se não estiver selecionado, adiciona a seleção
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+}
